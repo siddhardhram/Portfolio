d0ae1866e2c04ef7b0e3c9cb90754333025a2dcf
siddhardhram <ponnamandaram711@gmail.com>
2025-12-24 19:42:06 +0530

    Remove email gate from Contact page for direct access

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,10 +1,8 @@
 import Contact from '../components/Contact';
-import { EmailGate } from '../components/EmailGate';
 
 const ContactPage = () => {
     return (
         <div className="min-h-screen pt-20">
-            <EmailGate />
             <Contact />
         </div>
     );
