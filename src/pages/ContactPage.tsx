c9e61c62e3113406cf63e60246837c656f5aa495
siddhardhram <ponnamandaram711@gmail.com>
2025-12-24 19:25:10 +0530

    Add email gate to Contact page and fix Home navigation

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,8 +1,10 @@
 import Contact from '../components/Contact';
+import { EmailGate } from '../components/EmailGate';
 
 const ContactPage = () => {
     return (
         <div className="min-h-screen pt-20">
+            <EmailGate />
             <Contact />
         </div>
     );
