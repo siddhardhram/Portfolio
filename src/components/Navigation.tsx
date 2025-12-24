05f7f25d1ffb5a87239a2c66d5f4e8907720dd21
siddhardhram <ponnamandaram711@gmail.com>
2025-12-24 18:30:58 +0530

    Fix: Home button now scrolls to top of page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import { FloatingNav } from './ui/FloatingNavbar';
 
 const Navigation = () => {
   const navItems = [
-    { name: 'Home', link: '/', icon: <Home size={15} className="text-neutral-500 dark:text-neutral-300" /> },
+    { name: 'Home', link: '#hero', icon: <Home size={15} className="text-neutral-500 dark:text-neutral-300" /> },
     { name: 'GitHub', link: 'https://github.com/siddhardhram', icon: <Github size={15} className="text-neutral-500 dark:text-neutral-300" /> },
     { name: 'LinkedIn', link: 'https://linkedin.com/in/ponnamandasiddhardha', icon: <Linkedin size={15} className="text-neutral-500 dark:text-neutral-300" /> },
     { name: 'Contact', link: '/contact', icon: <Mail size={15} className="text-neutral-500 dark:text-neutral-300" /> },
